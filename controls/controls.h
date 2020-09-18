b485a069f06f8e06536c2f7016a50dc1aa8f7576
cmguo <gcm@ustc.edu>
2020-09-18 11:25:51 +0800

    disable test controls
    Change-Id: Icc0723c43c82be7cbdf318277d567812b70b2c4c

--- controls/controls.h
+++ controls/controls.h
@@ -14,17 +14,23 @@
 #include <qexport.h>
 
 REGISTER_CONTROL(ImageControl, "image,bmp,gif,jpg,jpeg,png")
+#ifdef QT_DEBUG
 REGISTER_CONTROL(VideoControl, "video,mp4,wmv,ts,flv,m3u8,asf,avi,"
                                "audio,mp3,wma,wav,"
                                "rtsp,rtmp,rtp")
 REGISTER_CONTROL(StrokeControl, "glstroke")
+#endif
 REGISTER_CONTROL(WebControl, "htm,html,http,https,chrome,swf")
+#ifdef QT_DEBUG
 REGISTER_CONTROL(QmlControl, "qml")
+#endif
 REGISTER_CONTROL(TextControl, "text,txt,js,cpp,h,qss,css")
 
 #ifdef WIN32
 REGISTER_CONTROL(PptxControl, "ppt,pptx")
+# ifdef QT_DEBUG
 REGISTER_CONTROL(WordControl, "doc,docx")
+# endif
 #endif
 
 #endif // CONTROLS_H
